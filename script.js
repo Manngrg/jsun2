497f60fed6f2a9464dc6c22fe03db376d8269faf
manoz <manbgrg18@gmail.com>
2026-01-28 16:12:59 +0545

    arraow functions

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ function calcAge1(birthYear) {
 age1 = calcAge1(2001);
 console.log(age1);
 
+// function expression ]
+
+
+// arrowfunction 
+
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const age3 = calcAge3(1990);
+console.log(age3);
+
+const yearsUntilRetire = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirementAge = 65 - age;
+    // return retirementAge;
+    return `${firstName} retires in ${retirementAge} years`
+}
+console.log(yearsUntilRetire(1991, 'manoj'));
+
+
+// if one statement no return required
+//if multiple parameters paranthesis is required for parameters and return too.
+
+
+
+
+
