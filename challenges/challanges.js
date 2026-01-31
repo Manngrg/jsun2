d06ff30bc39a06ec588ab7248612c24fe8b3ec53
manoz <manbgrg18@gmail.com>
2026-01-31 18:04:43 +0545

    tip calculator simple function

--- challenges/challanges.js
+++ challenges/challanges.js
@@ -93,3 +93,34 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 
 checkWinner(scoreDolphin, scoreKoalas);
 
+
+/* number 4 =>
+Steven wants to build a very simple tip calculator for whenever he goes eating in a 
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
+300. If the value is different, the tip is 20%. 
+Your tasks: 
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for 
+this. It's not allowed to use an if/else statement 
+�
+� (If it's easier for you, you can 
+start with an if/else statement, and then try to convert it to a ternary 
+operator!) 
+2. Print a string to the console containing the bill value, the tip, and the final value 
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 
+316.25” 
+Test data: 
+§ Data 1: Test for bill values 275, 40 and 430
+
+
+*/
+
+function tipCalculator(bill) {
+
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+}
+let bill = 900;
+let tip = tipCalculator(bill); // important to pass bill as argument.
+let sum = bill + tip;
+console.log(`The Bill is ${bill} and the tip is ${tip}, please pay total of ${sum}`);
+
