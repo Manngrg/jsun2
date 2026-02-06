923a4eb41c0ff504212b94ea5d089a2301469ba6
manoz <manbgrg18@gmail.com>
2026-02-06 14:34:57 +0545

    object solution no.3 added

--- objects1/obj.js
+++ objects1/obj.js
@@ -34,6 +34,9 @@ console.log(jonas['last' + nameKey]); //lastName is created.
 /*  why use bracket notation ?
 ->  when we need to 1st compute the property name like in the above.
 
+
+=> . notation is member accesss where as bracket is computed member access 
+because we can compute the property names we want to access. both are L -> R
 */
 
 const interestedIn = prompt('What do you want to know about Manoj? Choose between  firstName, lastName, age, job and friends');
@@ -54,3 +57,48 @@ console.log(jonas);
 // jonas has 3 friends and his best friend is hari
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`);
+
+// Object methods*
+
+const manoj = {
+    firstName: 'manoj',
+    lastName: 'gurung',
+    birthYear: 2001,
+    job: 'unemployed',
+    friends: ['mike', 'jay', 'katt'],
+    hasDriversLicense: false,
+
+    //     calcAge: function (birthYear) {
+    //         return 2026 - birthYear;
+    //     }
+    // };
+
+
+    calcAge: function (birthYear) {
+        return 2026 - this.birthYear;
+    },
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()} year old teacher, 
+    and he has ${this.hasDriversLicense ? 'a' : 'no'}
+     driver's license`
+    }
+};
+
+
+console.log(manoj.calcAge());
+//or
+// console.log(manoj['calcAge'](2001));
+
+// Manoj is a 46 year old teacher, and
+// he has a driver&s license.
+
+
+
+console.log(manoj.getSummary());
+
+
+
+
+
+
+
