95c1d6596acf29d41aaa531b4dcabe6f360c8039
manoz <manbgrg18@gmail.com>
2026-02-08 16:56:55 +0545

    looping arrays

--- arrays/array.js
+++ arrays/array.js
@@ -9,3 +9,27 @@ const friends2 = ['mike ', 'paypay', 'steven'];
 const newLength = friends2.push('newOne');
 console.log(friends2);
 console.log(newLength);
+
+const jonas = [
+    'jonas',
+    'Scmann',
+    2082 - 2057,
+    'teacher',
+    ['mike', 'peter', 'steven']
+];
+
+const types = [];
+
+
+
+// array are object.
+for (let i = 0; i < jonas.length; i++) {
+    // reading from jonas array
+    console.log(jonas[i], typeof jonas[i])
+
+    //filling types array
+    // types[i] = typeof jonas[i];
+
+    types.push(typeof jonas[i]);
+}
+console.log(types);
